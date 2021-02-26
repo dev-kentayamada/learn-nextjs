0eb937eda9275c4726c7e2deba11fd56809c702a
Kenta Yamada <kentayamada.tech@gmail.com>
2021-02-26 16:12:18 +0000

    Deploy back-end server to heroku

--- back-end/rest_api/settings.py
+++ back-end/rest_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["learn-nextjs-rest-api.herokuapp.com"]
 
 
 # Application definition
