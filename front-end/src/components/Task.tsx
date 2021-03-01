ed385285a1c211a43fae4164419e8964136f83e2
Kenta Yamada <kentayamada.tech@gmail.com>
2021-03-01 15:00:39 +0000

    deleteTask

--- front-end/src/components/Task.tsx
+++ front-end/src/components/Task.tsx
@@ -1,5 +1,8 @@
-import { Box } from '@material-ui/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Box, IconButton } from '@material-ui/core';
 import React from 'react';
+import Cookie from 'universal-cookie';
 import Link from './Link';
 
 type Task = {
@@ -12,7 +15,24 @@ type Props = {
   task: Task;
   taskDeleted: () => void;
 };
+
+const cookie = new Cookie();
+
 export const Task: React.FC<Props> = (props) => {
+  const deleteTask = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${props.task.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `JWT ${cookie.get('access_token')}`,
+      },
+    }).then((res) => {
+      if (res.status === 401) {
+        alert('JWT Token not valid');
+      }
+    });
+    props.taskDeleted();
+  };
   return (
     <>
       <Box>
@@ -23,6 +43,9 @@ export const Task: React.FC<Props> = (props) => {
         >
           {props.task.id}: {props.task.title}
         </Link>
+        <IconButton onClick={deleteTask}>
+          <FontAwesomeIcon icon={faTrash} />
+        </IconButton>
       </Box>
     </>
   );
