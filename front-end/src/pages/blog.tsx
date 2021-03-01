5376da78ae07bc2083e197fc60cdca56b3c2c938
Kenta Yamada <kentayamada.tech@gmail.com>
2021-03-01 04:52:33 +0000

    getStaticProps (Blog REST API)

--- front-end/src/pages/blog.tsx
+++ front-end/src/pages/blog.tsx
@@ -1,6 +1,47 @@
+import { Box } from '@material-ui/core';
+import { InferGetStaticPropsType } from 'next';
 import React from 'react';
+import { Layout } from '../components/Layout';
+import Link from '../components/Link';
 
-export default function Blog() {
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+};
 
-  return <>blog</>;
+export const getStaticProps = async (): Promise<{
+  props: {
+    filteredPosts: Post[];
+  };
+}> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-post/`);
+  const posts: Post[] = await res.json();
+  const filteredPosts = posts.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+  return {
+    props: {
+      filteredPosts,
+    },
+  };
+};
+
+export default function Blog({ filteredPosts }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  return (
+    <Layout title="blog">
+      <Box sx={{ textAlign: 'center' }}>
+        {filteredPosts.map((post, index) => (
+          <Box key={index}>
+            <Link
+              href={{
+                pathname: `/posts/${post.id}`,
+              }}
+            >
+              {post.id}: {post.title}
+            </Link>
+          </Box>
+        ))}
+      </Box>
+    </Layout>
+  );
 }
