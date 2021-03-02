d66530a99ee540d5a48d1f1ee2ed01949af07649
Kenta Yamada <kentayamada.tech@gmail.com>
2021-03-02 14:23:12 +0000

    update

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -24,7 +24,8 @@ export default function MyApp(props: AppProps): JSX.Element {
     <CacheProvider value={cache}>
       <Head>
         <title>Loading...</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
