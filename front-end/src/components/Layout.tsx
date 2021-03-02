63bbe4b5ae4a3f4641980d068b517b2acf2938b5
Kenta Yamada <kentayamada.tech@gmail.com>
2021-03-02 14:24:31 +0000

    update

--- front-end/src/components/Layout.tsx
+++ front-end/src/components/Layout.tsx
@@ -13,8 +13,6 @@ export const Layout = ({ children, title = 'Learn Next.Js' }: Props): JSX.Elemen
   <>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Box
       sx={{
