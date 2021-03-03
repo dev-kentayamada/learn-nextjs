17f606acaa14846963b7d57cbb4f181e69584f37
Kenta Yamada <kentayamada.tech@gmail.com>
2021-03-03 04:48:37 +0000

    change SSG to SSR

--- front-end/src/pages/blogs/index.tsx
+++ front-end/src/pages/blogs/index.tsx
@@ -1,52 +1,55 @@
-import { Box } from '@material-ui/core';
-import { InferGetStaticPropsType } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import React from 'react';
 import { Layout } from '../../components/Layout';
 import Link from '../../components/Link';
 
-type Post = {
+type Blog = {
   id: string;
   title: string;
   content: string;
   created_at: string;
 };
 
-export const getStaticProps = async (): Promise<{
-  props: {
-    filteredPosts: Post[];
-  };
-  revalidate: number;
-}> => {
+type Props = {
+  filteredBlogs: Blog[];
+};
+
+export const getServerSideProps: GetServerSideProps<Props, Blog> = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-post/`);
-  const posts: Post[] = await res.json();
-  const filteredPosts = posts.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+  if (res.status === 404) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  const blogs: Blog[] = await res.json();
+  const filteredBlogs = blogs.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+
   return {
     props: {
-      filteredPosts,
+      filteredBlogs,
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
-    revalidate: 1, // In seconds
   };
 };
 
-export default function Blogs({ filteredPosts }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+export default function Blogs({ filteredBlogs }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   return (
     <Layout title="Blogs">
-      <Box sx={{ textAlign: 'center' }}>
-        {filteredPosts.map((post, index) => (
-          <Box key={index}>
+      <ol>
+        {filteredBlogs.map((blog, index) => (
+          <li key={index}>
             <Link
               href={{
-                pathname: `/blogs/${post.id}`,
+                pathname: `/blogs/${blog.id}`,
               }}
             >
-              {post.id}: {post.title}
+              {blog.title}
             </Link>
-          </Box>
+          </li>
         ))}
-      </Box>
+      </ol>
     </Layout>
   );
 }
